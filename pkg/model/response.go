cc9db2599c1aaa32fd5b020be34561bf996f1a6b
yakob-abada <jakob@gymcatch.com>
2025-01-31 17:04:48 +0000

    remove unused file

---
+++ pkg/model/response.go
@@ -0,0 +1,9 @@
+package model
+
+type UserResponse struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Nickname  string `json:"nickname"`
+	Email     string `json:"email"`
+	Country   string `json:"country"`
+}
